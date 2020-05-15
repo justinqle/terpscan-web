a888f61c0a118b222a9f0ac82bed6e5db0a7d077
Justin Le <justinqle@gmail.com>
2020-05-15 03:13:04 -0400

    Modified predefined template with styling, separated components, added table

---
+++ src/components/TopBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app-bar
+    :clipped-left="$vuetify.breakpoint.lgAndUp"
+    app
+    elevation="1"
+    color="white"
+    light
+  >
+    <v-app-bar-nav-icon
+      @click.stop="$emit('toggle-drawer')"
+    ></v-app-bar-nav-icon>
+    <v-toolbar-title style="width: 210px" class="ml-0 pl-4">
+      <span class="hidden-sm-and-down">TerpScan</span>
+    </v-toolbar-title>
+    <v-text-field
+      flat
+      solo-inverted
+      hide-details
+      prepend-inner-icon="mdi-magnify"
+      label="Search"
+      class="hidden-sm-and-down"
+    ></v-text-field>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>mdi-inbox</v-icon>
+    </v-btn>
+    <v-btn icon large>
+      <v-avatar size="32px" item>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+          alt="Vuetify"
+        ></v-img>
+      </v-avatar>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-menu-down</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "TopBar",
+};
+</script>
+
+<style></style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,18 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#F44336',
+        secondary: '#F44336',
+        accent: '#F44336',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107',
+        background: '#F5F5F8',
+      },
+    },
+  },
 });
