876e2e35a5ef59269b0082b6bb6e050bb0dea11f
Justin Le <justinqle@gmail.com>
2020-06-30 03:44:30 -0400

    Custom scss, Navbar, Table

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,116 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Hi Justin,</h1>
+        <h2 class="subtitle">
+          you have
+          <strong>3</strong> packages ready for pickup.
+        </h2>
+      </div>
+      <br />
+      <div class="container">
+        <Table />
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar.vue";
+import Table from "./components/Table.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Navbar,
+    Table
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+// Import Bulma's core
+@import "~bulma/sass/utilities/_all";
+@import "~bulma/sass/layout/hero.sass";
+
+// Set your colors
+$primary: #ff4757;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099ff;
+$twitter-invert: findColorInvert($twitter);
+
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+  "white": (
+    $white,
+    $black
+  ),
+  "black": (
+    $black,
+    $white
+  ),
+  "light": (
+    $light,
+    $light-invert
+  ),
+  "dark": (
+    $dark,
+    $dark-invert
+  ),
+  "primary": (
+    $primary,
+    $primary-invert
+  ),
+  "info": (
+    $info,
+    $info-invert
+  ),
+  "success": (
+    $success,
+    $success-invert
+  ),
+  "warning": (
+    $warning,
+    $warning-invert
+  ),
+  "danger": (
+    $danger,
+    $danger-invert
+  ),
+  "twitter": (
+    $twitter,
+    $twitter-invert
+  )
+);
+
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+$navbar-item-active-color: #ff4757 !default;
+$navbar-breakpoint: $tablet !default;
+
+// $section-padding: 3rem 12rem !default;
+@include desktop {
+  section {
+    margin-left: 12rem;
+    margin-right: 12rem;
+  }
 }
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-navbar spaced shadow>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS2BkjifYFuSUOFkExtZB9Hw7IexmsOd957MQ&usqp=CAU"
+          alt="TerpScan logo"
+        />
+      </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <h1 class="subtitle is-4">TerpScan</h1>
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item href="#" active>Home</b-navbar-item>
+      <b-navbar-item href="#">About</b-navbar-item>
+    </template>
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Log in</strong>
+          </a>
+          <!-- <a class="button is-light">Log in</a> -->
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
